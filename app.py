9fd0a633638302d7b5dbee229f31810d657e08b3
ravib7 <ravibchaudhari7@gmail.com>
2026-06-03 18:57:12 +0530

    first flask server

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return {"message":"welcome to flask"}
+
+
+@app.route("/todo/create", methods=["POST"])
+def createTodo():
+    return {"message":"todo create success"}
+
+
+@app.route("/todos", methods=["GET"])
+def readTodo():
+    return {"message":"todo read success"}
+
+
+@app.route("/todo/update", methods=["PUT"])
+def updateTodo():
+    return {"message":"todo update success"}
+
+
+@app.route("/todo/remove", methods=["DELETE"])
+def deleteTodo():
+    return {"message":"todo remove success"}
+
+app.run(debug=True)
